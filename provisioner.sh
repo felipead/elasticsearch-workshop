53963fb0bd736a35c0be9dee62cfdb8952c26679
Alexandre Corrêa Barbosa <abarbos@thoughtworks.com>
2015-11-24 16:43:14 -0200

    Provisioning of elasticsearch

--- provisioner.sh
+++ provisioner.sh
@@ -33,6 +33,20 @@ install_java8() {
     fi
 }
 
+install_elasticsearch() {
+    if is_package_present elasticsearch; then
+        echo "Package elasticsearch already installed"
+    else
+        wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+        echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+        sudo apt-get -y update
+        install_package_if_not_present elasticsearch
+        sudo update-rc.d elasticsearch defaults 95 10
+        sudo service elasticsearch restart
+    fi
+}
+
 sudo apt-get -y update
 install_java8
+install_elasticsearch
 
